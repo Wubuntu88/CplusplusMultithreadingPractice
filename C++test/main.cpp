f66b3779833dbff7eea5df1f5ddf93a1ab9673b8
William Edward Gillespie <wgilles1@emich.edu>
2016-04-27 16:59:40 -0400

    Initial Commit

---
+++ C++test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++test
+//
+//  Created by William Edward Gillespie on 4/27/16.
+//  Copyright © 2016 William Edward Gillespie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
